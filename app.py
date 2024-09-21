20603484ea6e0abe9fe6e7a4622e5afe1aa67b1f
Nikrad Mahdi <1885199+nikrad@users.noreply.github.com>
2024-09-21 13:53:52 -0700

    Keep message history

--- app.py
+++ app.py
@@ -17,30 +17,35 @@ client = wrap_openai(openai.AsyncClient(api_key=API_KEY, base_url=ENDPOINT_URL))
 class RecommendationResponse(BaseModel):
     is_recommendation_query: bool
     product_type: str
+    max_price: int
 
 
 @traceable
 @cl.on_message
 async def handle_message(message):
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
     query = message.content
+    if not len(message_history) or message_history[0].get("role") != "system":
+        message_history.append({"role": "system", "content": INITIAL_SYSTEM_PROMPT})
+    message_history.append({"role": "user", "content": query})
     response = await client.beta.chat.completions.parse(
         model=MODEL,
-        messages=[
-            {
-                "role": "system",
-                "content": INITIAL_SYSTEM_PROMPT,
-            },
-            {"role": "user", "content": query},
-        ],
+        messages=message_history,
         response_format=RecommendationResponse,
+    )
+    message_history.append(
+        {"role": "assistant", "content": response.choices[0].message.content}
     )
     recommendation_response = response.choices[0].message.parsed
     print(f"is_recommendation_query: {recommendation_response.is_recommendation_query}")
     print(f"product_type: {recommendation_response.product_type}")
-
     if recommendation_response.is_recommendation_query:
         response_text = f"I recommend you buy the most expensive {recommendation_response.product_type} out there!"
     else:
         response_text = "Please suggest a type of product you would like to buy."
 
+    message_history.append({"role": "assistant", "content": response_text})
+    print(message_history)
+    cl.user_session.set("message_history", message_history)
     await cl.Message(content=response_text).send()
--- prompts.py
+++ prompts.py
@@ -1,3 +1,8 @@
 INITIAL_SYSTEM_PROMPT = """
-Determine if the user is asking for a product recommendation. Respond with a JSON object containing 'is_recommendation_query' (boolean) and 'product_type' (string).
+Determine if the user is asking for a product recommendation. 
+
+Respond with a JSON object containing:
+'is_recommendation_query' (boolean) 
+'product_type' (string)
+'max_price' (integer) if the user specifies a maximum price or None if the user does not specify a maximum price.
 """
